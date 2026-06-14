6b9be32284166becb0e8e4304911d70c1ce87ee3
Olzhas <cybertourist@github.com>
2026-06-15 00:20:44 +0500

    add bootstrap5 base template and navbar

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -34,10 +34,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_bootstrap5',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -51,7 +53,7 @@ ROOT_URLCONF = 'task_manager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("Привет от Hexlet-Code!")
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index'),
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
 ]
