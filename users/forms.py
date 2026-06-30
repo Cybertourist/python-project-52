937be87d0a22c61c1fc2cd7a654bfc8b9503580c
Olzhas <cybertourist@github.com>
2026-06-30 20:22:08 +0500

    Fix244

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
@@ -13,7 +13,12 @@ class CustomUserCreationForm(UserCreationForm):
         super().__init__(*args, **kwargs)
         self.fields['password1'].label = 'Пароль'
         self.fields['password2'].label = 'Подтверждение пароля'
-        
         self.fields['first_name'].label = 'Имя'
         self.fields['last_name'].label = 'Фамилия'
         self.fields['username'].label = 'Имя пользователя'
+
+class CustomAuthenticationForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].label = 'Имя пользователя'
+        self.fields['password'].label = 'Пароль'
